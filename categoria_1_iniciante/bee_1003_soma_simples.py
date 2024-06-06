90c0bde5751cc79f7762fdf0714af23b1ec3cb39
JotaP53 <martinsjp53@gmail.com>
2024-06-06 11:50:28 -0300

    dia3, 6 de junho de 2024

---
+++ categoria_1_iniciante/bee_1003_soma_simples.py
@@ -0,0 +1,8 @@
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print("SOMA =",SOMA)
+
+# Nota: Eu estava utilizando input(int()), e quando ia tentar responder a questão, apresentava erro. Quando coloquei int(input()) o erro foi resolvido. Não tenho certeza ainda, mas provavelmente a forma correta realmente seja int(input()).
