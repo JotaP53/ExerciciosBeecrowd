f2040e087f22056e7d0b4382bf034f27f6ac7eaf
JotaP53 <martinsjp53@gmail.com>
2024-06-07 17:39:22 -0300

    dia4, 7 de junho de 2024

---
+++ categoria_1_iniciante/bee_1008_salario.py
@@ -0,0 +1,8 @@
+number = int(input())
+number_hour = int(input())
+salary_hour = float(input())
+
+salary = number_hour * salary_hour
+
+print("NUMBER =", number)
+print("SALARY = U$ %.2f" % salary)
