ce0c7583d63b44da6f032dd9b4a6d5ab49cf5b8d
JotaP53 <martinsjp53@gmail.com>
2024-06-09 11:34:09 -0300

    atualizacao no exercicio 1015

--- categoria_1_iniciante/bee_1015_distancia_entre_dois_pontos.py
+++ categoria_1_iniciante/bee_1015_distancia_entre_dois_pontos.py
@@ -3,7 +3,18 @@ import math
 x1, y1 = input().split()
 x2, y2 = input().split()
 
-distancia = math.sqrt(((float(x2) - float(x1)) * (float(x2) - float(x1))) + ((
-    float(y2) - float(y1)) * (float(y2) - float(y1))))
+distancia = math.sqrt(((float(x2) - float(x1)) * (float(x2) - float(x1))) +
+                      ((float(y2) - float(y1)) * (float(y2) - float(y1))))
+
+print("%.4f" % distancia)
+
+
+# Outra maneira de escrever o código:
+
+x1, y1 = input().split()
+x2, y2 = input().split()
+
+distancia = (((float(x2) - float(x1)) ** 2) +
+             ((float(y2) - float(y1)) ** 2)) ** (1/2)
 
 print("%.4f" % distancia)
